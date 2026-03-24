4ee8ba5847c1e8bef501623e838d5170568a108d
Adrien Dupont <95227652+Adrienfdupont@users.noreply.github.com>
2026-03-24 18:30:37 +0100

    unflip card

--- src/card.py
+++ src/card.py
@@ -2,28 +2,48 @@ import os
 
 import pygame
 
-from settings import CARD_RATIO, WINDOW_HEIGHT
+from settings import CARD_HEIGHT, CARD_WIDTH, WINDOW_HEIGHT
 
 
 class Card(pygame.sprite.Sprite):
     instances = pygame.sprite.Group()
 
-    def __init__(self, rank, suit, cards, player):
+    def __init__(self, rank, suit, cards, player, isFlipped):
         super().__init__()
+        self.rank = rank
+        self.suit = suit
+        self.cards = cards
+        self.player = player
+        self.isFlipped = isFlipped
         Card.instances.add(self)
 
-        image_path = os.path.join("assets", "cards", f"{rank}{suit}.bmp")
-        image = pygame.image.load(image_path)
-        width, height = image.get_size()
-        self.image = pygame.transform.scale(image, (width * CARD_RATIO // height, CARD_RATIO))
-
-        if player == 1:
-            top = 10
+        if self.isFlipped:
+            self.load_image()
         else:
-            top = WINDOW_HEIGHT - self.image.get_height() - 10
-        left = 10 + ((self.image.get_width() + 10) * cards)
-        self.rect = self.image.get_rect()
-        self.rect.topleft = (int(left), top)
+            self.load_white_background()
+
+        self.set_position()
 
     def draw(self, screen):
         screen.blit(self.image, self.rect)
+
+    def load_image(self):
+        image_path = os.path.join("assets", "cards", f"{self.rank}{self.suit}.bmp")
+        image = pygame.image.load(image_path)
+        self.image = pygame.transform.scale(image, (CARD_WIDTH, CARD_HEIGHT))
+
+    def set_position(self):
+        if self.player == 1:
+            top = 10
+        else:
+            top = WINDOW_HEIGHT - self.image.get_height() - 10
+        left = 10 + ((self.image.get_width() + 10) * self.cards)
+        self.rect = self.image.get_rect()
+        self.rect.topleft = (int(left), top)
+
+    def load_white_background(self):
+        self.image = pygame.Surface((CARD_WIDTH, CARD_HEIGHT))
+        self.image.fill((255, 255, 255))
+
+    def show(self):
+        self.load_image()
